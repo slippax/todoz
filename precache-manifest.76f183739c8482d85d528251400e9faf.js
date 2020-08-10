f539475bd5ca13435b9e4af599c75f1890c433a1
Stephen Lippa <slippa@Stephens-MacBook-Pro.local>
2020-08-10 12:13:09 -0400

    Updates

---
+++ precache-manifest.76f183739c8482d85d528251400e9faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e998650da088ed7db343f3d85df0b9f",
+    "url": "/todoz/index.html"
+  },
+  {
+    "revision": "06629fe6ba4d20c15770",
+    "url": "/todoz/static/css/main.676d4562.chunk.css"
+  },
+  {
+    "revision": "aeb406ffb069a6cdce60",
+    "url": "/todoz/static/js/2.4d396e2e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todoz/static/js/2.4d396e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06629fe6ba4d20c15770",
+    "url": "/todoz/static/js/main.97d30250.chunk.js"
+  },
+  {
+    "revision": "e17c19493a5ca5bdf7f2",
+    "url": "/todoz/static/js/runtime-main.7cedab96.js"
+  }
+]);
