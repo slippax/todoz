c046ddbb4e6c3077ef0be9e091eddbe044a64608
Stephen Lippa <slippa@Stephens-MacBook-Pro.local>
2020-08-13 18:45:11 -0400

    Updates

---
+++ precache-manifest.0ffd2b0bf4ae20d139ca793b88addec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b886963632cb7f7081d673b0ea46aa8a",
+    "url": "/todoz/index.html"
+  },
+  {
+    "revision": "12339db34406c6532152",
+    "url": "/todoz/static/css/main.7bd8d4dc.chunk.css"
+  },
+  {
+    "revision": "f9acf3d96baab8c64171",
+    "url": "/todoz/static/js/2.dd496157.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todoz/static/js/2.dd496157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12339db34406c6532152",
+    "url": "/todoz/static/js/main.6f5330bb.chunk.js"
+  },
+  {
+    "revision": "e17c19493a5ca5bdf7f2",
+    "url": "/todoz/static/js/runtime-main.7cedab96.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/todoz/precache-manifest.06a0a07ee2dc599a6c58dbb93083911e.js"
+  "/todoz/precache-manifest.0ffd2b0bf4ae20d139ca793b88addec1.js"
 );
 
 self.addEventListener('message', (event) => {
