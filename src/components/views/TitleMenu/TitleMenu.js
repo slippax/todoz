477363630076ce455dad23960ded6e7dc25212b1
Stephen Lippa <slippa@Stephens-MacBook-Pro.local>
2020-08-13 13:23:45 -0400

    iphone ui fix

--- src/components/views/TitleMenu/TitleMenu.js
+++ src/components/views/TitleMenu/TitleMenu.js
@@ -1,10 +1,10 @@
 import React from "react";
 import classes from "./TitleMenu.module.css";
-import Bounce from "react-reveal/Zoom";
+import Bounce from "react-reveal/Roll";
 const TitleMenu = () => {
   return (
     <div className={classes.titleText}>
-      <Bounce cascade>
+      <Bounce top cascade>
         <h1>Todoz</h1>
         <p className={classes.descriptionText}>Add a todo below to begin</p>
       </Bounce>
